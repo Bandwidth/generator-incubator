6fa12c5733bb1413aee4f2705b52614e25f7d584
Tyler Cross <tcross@bandwidth.com>
2014-06-03 15:26:40 -0400

    Base file generation.
    Running `yo incubator` and then answering prompts will generate:
    - project files like package.json, Gruntfile.js
    - dotfiles like .travis.yml, .gitignore, .jshintrc, etc

---
+++ app/templates/_Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+
+	var sourceFiles = [ "*.js", "lib/**/*.js" ];
+	var testFiles   = [ "test/**/*.js" ];
+	var allFiles    = sourceFiles.concat(testFiles);
+
+	grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+		jscs: {
+			src: allFiles,
+			options: {
+				config: ".jscs.json"
+			}
+		},
+		jshint: {
+			jshintrc: ".jshint.json",
+			src: allFiles
+		},
+		mochaTest: {
+			test: {
+				options: {
+					reporter: "spec"
+				},
+				src: testFiles
+			}
+		},
+		coverage: {
+			options: {
+				thresholds: {
+					statements: 100,
+					branches: 100,
+					lines: 100,
+					functions: 100
+				},
+				dir: "coverage",
+				root: "test"
+			}
+		},
+		clean: [ "coverage" ]
+	});
+
+	// Load plugins
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-jscs-checker");
+	grunt.loadNpmTasks("grunt-mocha-test");
+	grunt.loadNpmTasks("grunt-istanbul-coverage");
+
+	// Rename plugins when needed
+	grunt.renameTask("coverage", "istanbul");
+
+	// Register tasks
+	grunt.registerTask("lint", "Check for common code problems.", [ "jshint" ]);
+	grunt.registerTask("style", "Check for style conformity.", [ "jscs" ]);
+	grunt.registerTask("test", "Run the test suite.", [ "mochaTest" ]);
+	grunt.registerTask("coverage", "Create a test coverage report.", [ "clean", "istanbul" ])
+
+	grunt.registerTask("default", [ "clean", "lint", "style", "test", "coverage" ]);
+
+}
---
+++ test/creation.js
@@ -0,0 +1,44 @@
+/*global describe, beforeEach, it */
+"use strict";
+var path = require("path");
+var helpers = require("yeoman-generator").test;
+
+describe("incubator generator", function () {
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, "temp"), function (err) {
+      if (err) {
+        return done(err);
+      }
+
+      this.app = helpers.createGenerator("incubator:app", [
+        "../../app"
+      ]);
+      done();
+    }.bind(this));
+  });
+
+  it("creates expected files", function (done) {
+    var expected = [
+      ".jshint.json",
+      ".jscs.json",
+      ".travis.yml",
+      ".editorconfig",
+      ".gitignore",
+      "package.json",
+      "Gruntfile.js"
+    ];
+
+    helpers.mockPrompt(this.app, {
+      projectName: "test-project",
+      projectDescription: "This is a test project",
+      projectGitRepoUrl: "git@github.com:inetCatapult/test-project.git"
+    });
+
+    this.app.options["skip-install"] = true;
+    
+    this.app.run({}, function () {
+      helpers.assertFile(expected);
+      done();
+    });
+  });
+});
